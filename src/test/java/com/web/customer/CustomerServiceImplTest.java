febad2c35ee3cd660ad2e9d97f2af4aff1b47b17
Hasinabanu M <hasin03m@gmail.com>
2022-03-21 15:12:14 +0530

    Customer Service Test file modified

--- src/test/java/com/web/customer/CustomerServiceImplTest.java
+++ src/test/java/com/web/customer/CustomerServiceImplTest.java
@@ -36,28 +36,18 @@ public class CustomerServiceImplTest {
 	private CustomerServiceImpl customerServiceImpl;
 	List<Customer> customerList;
 	private Customer customer1;
-	private Customer customer2;
 	
 	@BeforeEach
 	public void setUp() {
 		customerList = new ArrayList<>();
 		customer1 = new Customer(101L, "John", "Adams", "john_ad@gmail.com", 7890300021L, "34-Seven sisters St, London");
-		customer2 = new Customer(102L, "Anna", "Maria", "anna_22@gmail.com", 9844755473L, "20-WesternBorough,Edinburgh");
-		customerList.add(customer1);
-		customerList.add(customer2);
-	}
-	
-	private List<Customer> getCustomersList() {
-		List<Customer> list = new ArrayList<>();
-		list.add(new Customer(101L, "John", "Adams", "john_ad@gmail.com", 7890300021L, "34-Seven sisters St, London"));
-		list.add(new Customer(102L, "Anna", "Maria", "anna_22@gmail.com", 9844755473L, "20-WesternBorough,Edinburgh"));
-	    return list;	
+		customerList.add(customer1);		
 	}
 	
 	@Test
 	@Rollback(false)
 	public void saveCustomer() {
-		Customer customer = new Customer(101L, "John", "Adams", "john_ad@gmail.com", 7890300021L, "34-Seven sisters St, London");
+		Customer customer = new Customer(51L, "Princy", "Jose", "princy_ad@gmail.com", 7890300021L, "74,Forest Gate, London");
 		Customer savedCustomer = customerRepository.save(customer);
 		assertNotNull(savedCustomer);
     }
@@ -69,62 +59,4 @@ public class CustomerServiceImplTest {
 		assertThat(customers).size().isGreaterThan(0);
 	}
 	
-	
-	@Test
-	@Rollback(false)
-	public void deleteCustomerTest() {
-		Long id = (long) 1;
-		boolean isExistBeforeDelete = customerRepository.findById(id).isPresent();
-		
-		customerRepository.deleteById(id);
-		
-		boolean notExistAfterDelete = customerRepository.findById(id).isPresent();
-		
-		assertTrue(isExistBeforeDelete);
-		assertFalse(notExistAfterDelete);
-	}
-	
-	
-	
-	/*
-	 * @Test
-	 * 
-	 * @Rollback(false) public void updateCustomerTest() { Long id = 102L; Customer
-	 * customer = new Customer(102L, "Anna", "Maria", "anna_22@gmail.com",
-	 * 9844755473L, "20-WesternBorough,Edinburgh");
-	 * customerRepository.save(customer);
-	 * 
-	 * Optional<Customer> updateCustomer = customerRepository.findById(102L);
-	 * assertThat(updateCustomer.get().getId()).isEqualTo(id); }
-	 */
-
-	/*
-	 * @Test public void shouldReturnAllUsers() { List<Customer> users =
-	 * getCustomersList(); users.add(new Customer());
-	 * 
-	 * when(customerRepository.findAll()).thenReturn(users);
-	 * 
-	 * List<Customer> expected = customerServiceImpl.getAllCustomers();
-	 * 
-	 * assertEquals(expected, users); verify(customerRepository).findAll(); }
-	 */
-	
-	//@Test
-	//public void findAllCustomersTest() {
-		//STUB
-		//Mockito.when(customerRepository.findAll()).thenReturn(getCustomersList());
-		
-		//Action
-		//List<Customer> customerLst = customerServiceImpl.getAllCustomers();
-		
-		//customerRepository.save(customer1);
-		//when(customerRepository.findAll()).thenReturn(customerList);
-	//System.out.print("Customer lst===", customerList);
-		//List<Customer> customerLst = customerServiceImpl.getAllCustomers();		
-		//assertEquals(customerLst, customerList);
-		//Assertion
-		//assertEquals(102, customerLst.get(1).getId());
-		//assertEquals(2, customerLst.size());		
-	//}
-
 }
