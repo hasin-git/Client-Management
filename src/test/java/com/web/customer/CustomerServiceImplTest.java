b7304767d7b9858550b9e9eddc5657259bdeff5b
Hasinabanu M <hasin03m@gmail.com>
2022-03-24 15:33:58 +0530

    Customer Service Impl file Updated

--- src/test/java/com/web/customer/CustomerServiceImplTest.java
+++ src/test/java/com/web/customer/CustomerServiceImplTest.java
@@ -1,62 +1,82 @@
 package com.web.customer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
 
 import com.web.customer.model.Customer;
 import com.web.customer.repository.CustomerRepository;
 import com.web.customer.service.CustomerServiceImpl;
 
-//@RunWith(MockitoJUnitRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ExtendWith(MockitoExtension.class)
 public class CustomerServiceImplTest {
 		
 	//Dependencies will be mock
-	@Autowired
+	@Mock
 	private CustomerRepository customerRepository;
 	
 	//System under Test
+	@Autowired
 	@InjectMocks
 	private CustomerServiceImpl customerServiceImpl;
-	List<Customer> customerList;
+	
 	private Customer customer1;
+	private Customer customer2;
+	List<Customer> customerList;
 	
 	@BeforeEach
 	public void setUp() {
 		customerList = new ArrayList<>();
-		customer1 = new Customer(101L, "John", "Adams", "john_ad@gmail.com", 7890300021L, "34-Seven sisters St, London");
-		customerList.add(customer1);		
+		customer1 = new Customer(12L, "Princy", "Jose", "princy_ad@gmail.com", 9889970021L, "74,Forest Gate, Glassgow");
+		customer2 = new Customer(13L, "Ashley", "Munoz", "ashley_m@gmail.com", 8755609823L, "48,WentsWorth Road,Ireland");
+		customerList.add(customer1);
+		customerList.add(customer2);
 	}
 	
 	@Test
-	@Rollback(false)
-	public void saveCustomer() {
-		Customer customer = new Customer(51L, "Princy", "Jose", "princy_ad@gmail.com", 7890300021L, "74,Forest Gate, London");
-		Customer savedCustomer = customerRepository.save(customer);
-		assertNotNull(savedCustomer);
-    }
+	public void getAllCustomersTest() {
+		customerRepository.save(customer1);
+		when(customerRepository.findAll()).thenReturn(customerList);
+			
+		List<Customer> cusList = customerServiceImpl.getAllCustomers();
+		assertEquals(customerList, cusList);
+		verify(customerRepository, times(1)).save(customer1);
+		verify(customerRepository, times(1)).findAll();
+	}
 	
 	@Test
-	public void testListCustomers() {
-		List<Customer> customers = customerRepository.findAll();
+	public void saveCustomerTest() {
+		when(customerRepository.save(customer1)).thenReturn(customer1);
+	    
+	    customerServiceImpl.saveCustomer(customer1);
+	    
+	    verify(customerRepository,times(1)).save(customer1);
+	}	
+	
+	@Test
+	public void getCustomerByIdTest() {
+		when(customerRepository.findById((long) 12)).thenReturn(Optional.ofNullable(customer1));
+		assertThat(customerServiceImpl.getCustomerById(customer1.getId())).isEqualTo(customer1);
+		verify(customerRepository, times(1)).findById(customer1.getId());
+	}	
 		
-		assertThat(customers).size().isGreaterThan(0);
-	}
-	
+	@Test
+	public void deleteCustomerByIdTest() {
+		customerServiceImpl.deleteCustomerById(customer1.getId());
+		verify(customerRepository, times(1)).deleteById(customer1.getId());
+	}	
 }
